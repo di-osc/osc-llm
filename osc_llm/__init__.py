9fd6d16b4182c01c70ee0525ea630f8b60142ab8
wangmengdi <790990241@qq.com>
2024-04-30 08:51:30 +0800

    增加engines

--- osc_llm/__init__.py
+++ osc_llm/__init__.py
@@ -1,8 +1,10 @@
 from .layers import *
 from .architectures import *
-from .tokenizer import Tokenizer
-from .config import registry, Config
 from .model_helpers import *
 from .quantizers import *
 from .chat_templates import *
 from .samplers import *
+from .engines import *
+
+from .tokenizer import Tokenizer
+from .config import registry, Config
