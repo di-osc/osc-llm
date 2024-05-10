695d1e1275ac9939b2d7d2c44d25094219662161
wangmengdi <790990241@qq.com>
2024-05-11 00:26:29 +0800

    SparseMoE -> SparseMoe

--- osc_llm/layers/__init__.py
+++ osc_llm/layers/__init__.py
@@ -3,7 +3,7 @@ from .attention import CausalSelfAttention
 from .head import LMHead
 from .linear import Linear, WeightOnlyInt4Linear, Int8Linear
 from .embedding import TokenEmbedding, TokenEmbeddingPlus
-from .feedforward import GLU, SwiGLU, GeGLU, SparseMoE
+from .feedforward import GLU, SwiGLU, GeGLU, SparseMoe
 from .activation import ReLU, SiLU, GELU
 from .kv_cache import StaticKVCache
 from .dropout import Dropout
@@ -27,5 +27,5 @@ __all__ = [
     "WeightOnlyInt4Linear",
     "Int8Linear",
     "Dropout",
-    "SparseMoE"
+    "SparseMoe"
 ]
