36e8c79fef7765be8400b50106bf07794e3ec883
wangmengdi <790990241@qq.com>
2024-05-14 08:58:46 +0800

    添加了SparseMoe的其他实现方式

--- osc_llm/layers/feedforward.py
+++ osc_llm/layers/feedforward.py
@@ -47,8 +47,8 @@ def SwiGLU(n_in: int,
                up_bias=up_bias,
                gate_bias=gate_bias,
                down_bias=down_bias)
-    
-
+           
+           
 @registry.layers.register("SwiGLU.v2")
 class SwiGLUV2(nn.Module):
     """Swish激活函数的门控线性单元另外一种实现方式,
@@ -90,10 +90,12 @@ def GeGLU(n_in: int,
                up_bias=up_bias,
                gate_bias=gate_bias,
                down_bias=down_bias)
-        
+    
     
 @registry.layers.register("SparseMoe")
 class SparseMoe(nn.Module):
+    """稀疏的专家混合网络
+    """
     def __init__(
         self,
         n_experts: int, 
@@ -141,3 +143,79 @@ class SparseMoe(nn.Module):
             y += self.shared_expert(x) * self.shared_gate(x).sigmoid()
             
         return y.reshape(B, T, C)
+    
+    
+@registry.layers.register("Experts.SwiGLU")
+class SwiGLUExperts(nn.Module):
+    """专家类型为SwiGLU的稀疏专家混合网络
+    """
+    def __init__(
+        self,
+        n_experts: int,
+        n_in: int,
+        n_hidden: int,
+    ):
+        self.up_proj = torch.nn.Parameter(torch.empty(n_experts, n_hidden, n_in))
+        self.gate_proj = torch.nn.Parameter(torch.empty(n_experts, n_hidden, n_in))
+        self.down_proj = torch.nn.Parameter(torch.empty(n_experts, n_in, n_hidden))
+        
+    def forward(self, x: torch.Tensor, expert_indices: torch.Tensor) -> torch.Tensor:
+        """
+        x: (tokens, n_in) == (T, D)
+        expert_indices: (tokens, n_active_experts) == (T, A)
+        
+        """
+        w_up = self.up_proj[expert_indices] # (T, A, H, D)
+        w_gate = self.gate_proj[expert_indices] # (T, A, H, D)
+        w_down = self.down_proj[expert_indices] # (T, A, D, H)
+        
+        x_gate = F.silu(torch.einsum("td, tahd -> tah", x, w_gate))
+        x_up = torch.einsum("td, tahd -> tah", x, w_up)
+        x_down = torch.einsum("tah, tadh -> tad", x_gate * x_up, w_down)
+        
+        return x_down
+    
+
+@registry.layers.register("SparseMoe.SwiGLU")
+class SwiGLUSparseMoe(nn.Module):
+    """稀疏专家混合网络的另一种实现方式
+    """
+    def __init__(
+        self,
+        n_experts: int,
+        n_activated_experts: int,
+        n_in: int,
+        n_hidden: int,
+        add_shared_expert: bool = False,
+        bias: bool = False,
+        norm_probs: bool = True
+    ):
+        super().__init__()
+        
+        self.experts = SwiGLUExperts(n_experts=n_experts, n_in=n_in, n_hidden=n_hidden)
+        self.n_activated_experts = n_activated_experts
+        self.gate = nn.Linear(n_in, n_experts)
+        self.add_shared_expert = add_shared_expert
+        self.norm_probs = norm_probs
+        
+        if add_shared_expert:
+            self.shared_expert = SwiGLU(n_in=n_in, n_hidden=n_hidden, up_bias=bias, down_bias=bias, gate_bias=bias)
+            self.shared_gate = nn.Linear(n_in, 1)
+        
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        B, S, D = x.size()
+        x = x.view(-1, D)
+        
+        logits = self.gate(x)  # (B*T, n_expert)
+        probs = F.softmax(logits, dim=-1, dtype=torch.float)
+        probs, indices = torch.topk(probs, self.n_activated_experts, dim=-1)  # probs: (B*T, n_expert_per_token), indices: (B*T, n_expert_per_token)
+        if self.norm_probs:
+            probs /= probs.sum(dim=-1, keepdim=True)
+        probs = probs.to(dtype=x.dtype)
+        
+        y = self.experts(x, indices)
+        
+        if self.add_shared_expert:
+            y += self.shared_expert(x) * self.shared_gate(x).sigmoid()
+        
+        return y.reshape(B, S, D)
