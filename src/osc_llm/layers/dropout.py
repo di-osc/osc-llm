1a86a8734321ba9102330320873d5dc425189712
善若水 <790990241@qq.com>
2025-03-04 10:58:20 +0000

    移除src

--- src/osc_llm/layers/dropout.py
+++
@@ -1,7 +0,0 @@
-from ..config import registry
-import torch.nn as nn
-
-
-@registry.layers.register("Dropout")
-def Dropout(p: float = 0.1, inplace: bool = False):
-    return nn.Dropout(p=p, inplace=inplace)
--- src/osc_llm/quantizers/__init__.py
+++
@@ -1,4 +0,0 @@
-from .int4 import WeightOnlyInt4Quantizer
-from .int8 import Int8Quantizer
-
-__all__ = ["WeightOnlyInt4Quantizer", "Int8Quantizer"]
