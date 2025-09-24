dea18926169db1bd63dd1cf655223c651996ea3f
善若水 <790990241@qq.com>
2025-09-24 12:06:26 +0000

    在__init__.py中新增SamplingParams导入，并将其添加到__all__列表中，以便于模块的使用和扩展。

--- osc_llm/__init__.py
+++ osc_llm/__init__.py
@@ -1,5 +1,6 @@
 # ruff: noqa
+from osc_transformers import SamplingParams
 from .models import Qwen3ForCausalLM
 from .chat_templates import Message
 
-__all__ = ["Qwen3ForCausalLM", "Message"]
+__all__ = ["Qwen3ForCausalLM", "Message", "SamplingParams"]
