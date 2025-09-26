a2d0f58fc0f160379f1fd1172588f7079082a789
善若水 <790990241@qq.com>
2025-09-26 23:11:16 +0000

    Remove download_model function and related imports from __main__.py, simplifying the command structure.

--- osc_llm/__main__.py
+++ osc_llm/__main__.py
@@ -1,52 +1,7 @@
-import os
-from pathlib import Path
-from typing import Literal, Optional
-
 from jsonargparse import CLI
 
 
-def download_model(
-    repo_id: str,
-    save_dir: str = "./checkpoints",
-    force_download: bool = False,
-    access_token: Optional[str] = os.getenv("HF_TOKEN"),
-    endpoint: Literal["hf", "hf-mirror", "modelscope"] = "hf-mirror",
-):
-    """
-    Download a model from the Hugging Face Hub or ModelScope.
-
-    Args:
-        repo_id: The ID of the model to download.
-        save_dir: The directory to save the downloaded model.
-        force_download: Whether to force the download of the model.
-        access_token: The access token to use for the Hugging Face Hub.
-        endpoint: The endpoint to use for the Hugging Face Hub.
-    """
-    directory = Path(save_dir, repo_id)
-
-    if endpoint == "modelscope":
-        from modelscope import snapshot_download
-
-        snapshot_download(
-            repo_id=repo_id,
-            local_dir=directory,
-        )
-    else:
-        if endpoint == "hf-mirror":
-            os.environ["HF_ENDPOINT"] = "https://hf-mirror.com"
-        from huggingface_hub import snapshot_download
-
-        snapshot_download(
-            repo_id,
-            local_dir=directory,
-            force_download=force_download,
-            token=access_token,
-        )
-
-
-commands = {
-    "download": download_model,
-}
+commands = {}
 
 
 def run_cli():
