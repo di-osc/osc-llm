afd789597647bb922de7cda797902dc49239fd8e
善若水 <790990241@qq.com>
2025-09-28 09:55:19 +0000

    chat cli

--- osc_llm/__main__.py
+++ osc_llm/__main__.py
@@ -1,7 +1,39 @@
 from jsonargparse import CLI
 
+from osc_llm import LLM
 
-commands = {}
+
+def chat(
+    checkpoint_dir: str,
+    gpu_memory_utilization: float = 0.5,
+    device: str = "cuda",
+    enable_thinking: bool = True,
+):
+    """chat with a model
+
+    Args:
+        checkpoint_dir (str): the path to the checkpoint directory
+        gpu_memory_utilization (float, optional): the GPU memory utilization. Defaults to 0.5.
+        device (str, optional): the device to use. Defaults to "cuda".
+    """
+    llm = LLM(checkpoint_dir, gpu_memory_utilization, device)
+    messages = []
+    while True:
+        user_input = input("Input: ")
+        if user_input == "exit":
+            break
+        messages.append({"role": "user", "content": user_input})
+        answer = ""
+        for token in llm.chat(messages, enable_thinking=enable_thinking, stream=True):
+            answer += token
+            print(token, end="", flush=True)
+        messages.append({"role": "assistant", "content": answer})
+        print()
+
+
+commands = {
+    "chat": chat,
+}
 
 
 def run_cli():
